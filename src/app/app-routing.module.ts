990e44deb8af5707062798205890c0c47f0aa3fa
Madinator <lvpjmd@gmail.com>
2022-07-13 14:51:03 +0600

    bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
+import { CoreModule } from './core/core.module';
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./core/public/public.module').then(m => m.PublicModule) }];
+const routes: Routes = [
+  { path: '', loadComponent: () => import('./core/pages/login/login.component').then(m => m.LoginComponent) },
+  { path: 'Courses', loadComponent: () => import('./core/pages/courses-list/courses-list.component').then(m => m.CoursesListComponent) },
+  { path: 'New-Course', loadChildren: () => import('./core/pages/courses-list/add-edit/add-edit.component').then(m => m.AddEditComponent) },
+  { path: '**', loadComponent: () => import('./core/pages/login/login.component').then(m => m.LoginComponent) }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CoreModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/core/components/components.module.ts
@@ -0,0 +1,18 @@
+import { BaseButtonComponent } from './base-button/base-button.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    BaseButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    BaseButtonComponent
+  ]
+})
+export class ComponentsModule { }
